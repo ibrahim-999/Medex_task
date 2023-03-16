988f4547923b6880ba20c605b4a81f1aa31449eb
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2023-03-16 21:39:46 +0200

    Adjusted category module

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CategoryResource;
+use App\Http\Resources\PaginatedCollection;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        $categories = QueryBuilder::for(Category::class)
+            ->defaultSort('-created_at')
+            ->latest()
+            ->paginate()
+            ->withQueryString();
+
+        $props = [
+            'brands' => new PaginatedCollection($categories, CategoryResource::class),
+        ];
+
+        return view('category');
+    }
 }
