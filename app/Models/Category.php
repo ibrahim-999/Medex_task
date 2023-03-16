098362eb1063fb332f208f4a83505ddfaf3eb424
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2023-03-16 19:37:59 +0200

    Add models relations

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Database\Factories\BrandFactory;
 use Database\Factories\CategoryFactory;
+use Database\Factories\ProductFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use function Spiral\Tokenizer\Reflection\registerUse;
 
 class Category extends Model
 {
@@ -18,5 +22,35 @@ class Category extends Model
     protected static function newFactory(): CategoryFactory
     {
         return CategoryFactory::new();
+    }
+
+    public function subcategories(): HasMany
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function parentCategory(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    public function isRoot(): bool
+    {
+        return $this->parent_id == null;
+    }
+
+    public function isParent(): bool
+    {
+        return $this->subcategories()->count() > 0;
+    }
+
+    public function isSubcategory(): bool
+    {
+        return $this->parent_id != null;
+    }
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class, 'subcategory_id');
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use BrandScope;
+use CategoryScope;
 use Database\Factories\CategoryFactory;
 use Database\Factories\ProductFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use SubcategoryScope;
 
 class Product extends Model
 {
@@ -26,4 +30,23 @@ class Product extends Model
     {
         return ProductFactory::new();
     }
+
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function subcategory(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    protected static function booted()
+    {
+        static::addGlobalScope(new CategoryScope);
+        static::addGlobalScope(new BrandScope);
+        static::addGlobalScope(new SubcategoryScope);
+    }
+
+
 }
