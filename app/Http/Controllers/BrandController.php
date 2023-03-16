ea67182044cce032c19cbdd8f630b77763a5964b
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2023-03-16 21:39:11 +0200

    Adjusted brand module

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\BrandResource;
+use App\Http\Resources\PaginatedCollection;
+use App\Models\Brand;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class BrandController extends Controller
 {
-    //
+    public function index()
+    {
+        $brands = QueryBuilder::for(Brand::class)
+            ->defaultSort('-created_at')
+            ->latest()
+            ->paginate()
+            ->withQueryString();
+
+        $props = [
+            'brands' => new PaginatedCollection($brands, BrandResource::class),
+        ];
+
+        return view('brand');
+    }
 }
--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -22,6 +22,6 @@ class Brand extends Model
 
     public function products(): HasMany
     {
-        return $this->hasMany(Product::class, 'brand_id');
+        return $this->hasMany(Product::class);
     }
 }
